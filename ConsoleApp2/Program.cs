882b097e22eab975c4a3120e9dffcb79ba6d3bf5
kasturipratik <pratik.parajuli@yahoo.com>
2018-08-26 17:51:50 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+     
+            Student a = new Student(1, "Pratik", "baltimore", "4102459450");
+            Student b = new Student(2, "Pratap", "Maryland", "4102454436");
+            arrayList.Add(a);
+            arrayList.Add(b);
+            foreach(Student st in arrayList)
+            {
+                Console.WriteLine(st);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Student
+    {
+        private int id;
+        private string name;
+        private string address;
+        private string phone;
+        public Student() {
+           
+        }
+
+        public Student(int id, string name, string address, string phone)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Address { get => address; set => address = value; }
+        public string Phone { get => phone; set => phone = value; }
+
+        public override string ToString()
+        {
+            return "{ ID: " + id + " Name: " + name + " Address: " + address + " Phone: " + phone + " }";
+        }
+    }
+}
